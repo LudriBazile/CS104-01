a2bc9aa52e322347938256bc0fd329bc19ffd2e1
LudriBazile <71726106+LudriBazile@users.noreply.github.com>
2020-09-29 14:36:36 -0400

    This is my second commit of my average calculator.

---
+++ Average Calulator 2.0.py
@@ -0,0 +1,10 @@
+numberOfTest = 0
+score = 0
+average = 0.0
+scoreCount = 0
+
+numberOfTests = int (input("Please enter the number of test you want to average"))
+score = int(input( "Please enter a score: "))
+scoreCount=ScoreCount+1               
+total = total + score
+average =total/scoCounter
